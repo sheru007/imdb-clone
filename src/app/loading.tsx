a7c4718925eb4106b83e8c8335d74dda5c7b0b75
sheru007 <jnvh1233sheru@gmail.com>
2024-03-15 16:06:02 +0530

    loading effect is done

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function loading() {
+  return (
+    <div className='flex justify-center mt-16'>
+        <img className='h-52' src='spinner.svg' alt='loading...' />
+    </div>
+  )
+}
+
+export default loading
